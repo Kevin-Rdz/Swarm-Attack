fa54619916ad0363557e5120a35182c5c1b2c3c7
Francisco Issac <a276679@alumnos.uaslp.mx>
2022-05-19 19:09:52 -0500

    add_more_classes

--- Characters.java
+++ Characters.java
@@ -38,5 +38,5 @@ public abstract class Characters extends Actor
         setLocation(getX(), getY() + fallSpeed);
         fallSpeed += ACCELERATION_G;
     }
-    
+
 }
--- Crawlid.java
+++ Crawlid.java
@@ -2,6 +2,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Crawlid extends Enemy
 {
+    private int attack = 1;
+    
+    public int getAttack()
+    {
+        return attack;
+    }
+    
     public int getDirection()
     {
         return 1;
